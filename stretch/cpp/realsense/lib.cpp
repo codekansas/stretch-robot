a97880f570afbdbaca1b6785753ecf67775e487f
Benjamin Bolte <ben@bolte.cc>
2022-10-24 04:27:03 +0000

    realsense reading library working ok

--- stretch/cpp/realsense/lib.cpp
+++ stretch/cpp/realsense/lib.cpp
@@ -7,37 +7,75 @@ int device_count() {
     return ctx.query_devices().size();
 }
 
-int main() try {
-    rs2::pipeline p;
-    std::cout << "a\n";
-    p.start();
-    std::cout << "b\n";
-    while (true) {
-        std::cout << "c\n";
-        rs2::frameset frames = p.wait_for_frames();
-        std::cout << "d\n";
-        rs2::depth_frame depth = frames.get_depth_frame();
-        std::cout << "e\n";
-        auto width = depth.get_width();
-        std::cout << "f\n";
-        auto height = depth.get_height();
-        std::cout << "g\n";
-        float dist_to_center = depth.get_distance(width / 2, height / 2);
-        std::cout << "The camera is facing an object " << dist_to_center << " meters away \r";
+class VideoFrame {
+   private:
+    rs2::video_frame rs_frame;
+
+   public:
+    VideoFrame(const rs2::video_frame& rs_frame) : rs_frame(std::move(rs_frame)) {}
+
+    const uint8_t* data() const { return (uint8_t*)rs_frame.get_data(); }
+    const int rows() const { return std::max(rs_frame.get_height(), 0); }
+    const int cols() const { return std::max(rs_frame.get_width(), 0); }
+    const int bytes_per_pixel() const { return rs_frame.get_bytes_per_pixel(); }
+};
+
+class Frame {
+   public:
+    VideoFrame rgb;
+
+    Frame(const VideoFrame& rgb) : rgb(std::move(rgb)) {}
+};
+
+class FrameGenerator {
+   private:
+    rs2::pipeline* pipe;
+
+   public:
+    FrameGenerator() : pipe(nullptr) {}
+
+    ~FrameGenerator() {
+        if (this->pipe != nullptr) this->pipe->stop();
     }
-    return EXIT_SUCCESS;
-} catch (const rs2::error& e) {
-    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    "
-              << e.what() << std::endl;
-    return EXIT_FAILURE;
-} catch (const std::exception& e) {
-    std::cerr << "True exception:" << e.what() << std::endl;
-    return EXIT_FAILURE;
-}
+
+    const FrameGenerator* iter() {
+        this->pipe = new rs2::pipeline();
+        this->pipe->start();
+        return this;
+    }
+
+    const Frame* next() {
+        if (this->pipe == nullptr)
+            throw std::runtime_error("`pipe` hasn't been initialized; must call `__iter__` first");
+        rs2::frameset frames = this->pipe->wait_for_frames();
+        VideoFrame video_frame(frames.get_color_frame());
+        Frame* frame = new Frame(video_frame);
+        return frame;
+    }
+};
 
 PYBIND11_MODULE(lib, m) {
     m.def("device_count", &device_count);
-    m.def("test_realsense_device", &main);
+
+    pybind11::class_<VideoFrame>(m, "VideoFrame", pybind11::buffer_protocol())
+        .def_buffer([](VideoFrame& m) -> pybind11::buffer_info {
+            auto buf = pybind11::buffer_info(m.data(), sizeof(uint8_t), true);
+            buf.format = pybind11::format_descriptor<uint8_t>::format();
+            buf.ndim = 3;
+            buf.shape = {m.rows(), m.cols(), m.bytes_per_pixel()};
+            buf.strides = {m.cols() * m.bytes_per_pixel(), m.bytes_per_pixel(), 1};
+            return buf;
+        })
+        .def_property_readonly("rows", &VideoFrame::rows)
+        .def_property_readonly("cols", &VideoFrame::cols)
+        .def_property_readonly("bytes_per_pixel", &VideoFrame::bytes_per_pixel);
+
+    pybind11::class_<Frame>(m, "Frame").def_readonly("rgb", &Frame::rgb);
+
+    pybind11::class_<FrameGenerator>(m, "FrameGenerator")
+        .def(pybind11::init<>())
+        .def("__iter__", &FrameGenerator::iter)
+        .def("__next__", &FrameGenerator::next);
 }
 
 }  // namespace stretch::realsense
--- tests/test_realsense.py
+++ tests/test_realsense.py
@@ -1,8 +1,17 @@
-import pytest
+import itertools
 
+import numpy as np
+import pytest
+from PIL import Image
+
+from stretch.cpp.realsense import FrameGenerator
 from stretch.cpp.realsense.lib import device_count
 
 
 @pytest.mark.realsense
 def test_realsense() -> None:
     assert device_count() > 0
+    for frame in itertools.islice(FrameGenerator(), 3):
+        arr = np.array(frame.rgb, copy=False)
+        img = Image.fromarray(arr)
+        assert img.size > (0, 0)
