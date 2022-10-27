ec278064f92f2fd61844d1c8677195d6aa7c32ae
Benjamin Bolte <ben@bolte.cc>
2022-10-27 15:22:05 -0400

    better implementation for streaming from the realsense camera

--- stretch/backend/camera.py
+++ stretch/backend/camera.py
@@ -1,10 +1,12 @@
 import asyncio
 import errno
+import functools
 import io
 import logging
 import math
 import platform
-from typing import Any, AsyncIterable, Dict, Literal, Optional, Set, cast, get_args
+from dataclasses import dataclass
+from typing import AsyncIterable, Dict, Iterable, Literal, Optional, Set, cast, get_args
 
 import av
 import numpy as np
@@ -24,6 +26,19 @@ router = r = APIRouter()
 CameraType = Literal["depth", "rgb"]
 
 
+@dataclass(frozen=True)
+class Extrinsics:
+    rotation: np.ndarray
+    position: np.ndarray
+
+
+@dataclass(frozen=True)
+class Frame:
+    rgb: av.video.frame.VideoFrame
+    depth: av.video.frame.VideoFrame
+    extrinsics: Extrinsics
+
+
 def cast_camera_type(raw_camera_type: str) -> CameraType:
     args = get_args(CameraType)
     assert raw_camera_type in args, f"Invalid camera type: {raw_camera_type}"
@@ -36,7 +51,7 @@ async def iter_webcam_frames(
     width: int = 640,
     height: int = 480,
     pixel_format: str = "yuyv422",
-) -> AsyncIterable[av.video.frame.VideoFrame]:
+) -> AsyncIterable[Frame]:
     system = platform.system()
     assert system == "Darwin", f"Unsupported {system=}"
 
@@ -50,6 +65,12 @@ async def iter_webcam_frames(
         "video_size": f"{width}x{height}",
         "pixel_format": pixel_format,
     }
+
+    # Use some dummy extrinsics, since the webcam doesn't have any.
+    dummy_extrinsics = Extrinsics(
+        rotation=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.float64),
+        position=np.array([0, 0, 0], dtype=np.float64),
+    )
 
     try:
         container = av.open(file=av_file, format=av_fmt, mode="r", options=options, timeout=None)
@@ -68,7 +89,8 @@ async def iter_webcam_frames(
             if video_first_pts is None:
                 video_first_pts = frame.pts
             frame.pts -= video_first_pts
-            yield frame
+
+            yield Frame(rgb=frame, depth=frame, extrinsics=dummy_extrinsics)
 
     except Exception:
         logger.exception("Caught exception while iterating webcam frames")
@@ -77,102 +99,125 @@ async def iter_webcam_frames(
         container.close()
 
 
-def get_cpp_camera_type(camera_type: CameraType) -> Any:
-    match camera_type:
-        case "rgb":
-            return realsense_lib.CameraType.rgb
-        case "depth":
-            return realsense_lib.CameraType.depth
-    raise KeyError(camera_type)
+async def iter_realsense_frames() -> AsyncIterable[Frame]:
+    assert realsense_lib is not None
+
+    # Use some dummy extrinsics for now; still need to read them from device.
+    dummy_extrinsics = Extrinsics(
+        rotation=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.float64),
+        position=np.array([0, 0, 0], dtype=np.float64),
+    )
+
+    for frame in realsense_lib.FrameGenerator():
+        rgb_arr = np.array(frame.rgb, copy=False)
+        depth_arr = np.array(frame.depth, copy=False)
+
+        depth_arr = depth_arr[..., 1].astype(np.float64) * 256.0 + depth_arr[..., 0].astype(np.float64)
+        # Normal scaling.
+        # depth_arr = (depth_arr / 256).astype(np.uint8)
+        # Inverse depth scaling.
+        depth_arr = (255 * 1024 / (depth_arr + 1024)).astype(np.uint8)
+
+        rgb_frame = av.video.frame.VideoFrame.from_ndarray(rgb_arr, format="yuyv422")
+        depth_frame = av.video.frame.VideoFrame.from_ndarray(depth_arr, format="gray")
+
+        yield Frame(
+            rgb=rgb_frame,
+            depth=depth_frame,
+            extrinsics=dummy_extrinsics,
+        )
+
+        await asyncio.sleep(0.01)
 
 
-def get_camera_format(camera_type: CameraType) -> str:
-    match camera_type:
-        case "rgb":
-            return "yuyv422"
-        case "depth":
-            # After remapping colors.
-            # return "rgb24"
-            # Without remapping colors.
-            return "gray"
-    raise KeyError(camera_type)
-
-
-async def iter_realsense_frames(camera_type: CameraType) -> AsyncIterable[av.video.frame.VideoFrame]:
+async def iter_frames() -> AsyncIterable[Frame]:
     if realsense_lib is None:
         webcam_iter = iter_webcam_frames()
         async for frame in webcam_iter:
             yield frame
-        return
 
-    for frame in realsense_lib.FrameGenerator(get_cpp_camera_type(camera_type)):
-        arr = np.array(frame, copy=False)
-
-        # Can use `matplotlib.cm` to remap depths to RGB. Need to change camera format above.
-        # if camera_type == "depth":
-        #     arr = arr[..., 1].astype(np.float64) * 255.0 + arr[..., 0].astype(np.float64)
-        #     arr = (cm.jet(arr)[..., :3] * 255.0).astype(np.uint8)
-
-        if camera_type == "depth":
-            arr = arr[..., 1].astype(np.float64) * 255.0 + arr[..., 0].astype(np.float64)
-            # Normal scaling.
-            # arr = (arr / 256).astype(np.uint8)
-            # Inverse depth scaling.
-            arr = (255 * 1024 / (arr + 1024)).astype(np.uint8)
-
-        av_frame = av.video.frame.VideoFrame.from_ndarray(arr, format=get_camera_format(camera_type))
-        yield av_frame
+    else:
+        realsense_iter = iter_realsense_frames()
+        async for frame in realsense_iter:
+            yield frame
 
 
 class ConnectionManager:
     def __init__(self) -> None:
         self.web_sockets: Dict[CameraType, Set[WebSocket]] = {}
         self.started = asyncio.Event()
+        self.lock = asyncio.Lock()
         self.task = asyncio.create_task(run_connection_manager(self))
 
     async def connect(self, camera_type: CameraType, websocket: WebSocket) -> None:
         await websocket.accept()
+        await self.lock.acquire()
         if camera_type in self.web_sockets:
             self.web_sockets[camera_type].add(websocket)
         else:
             self.web_sockets[camera_type] = {websocket}
+        self.lock.release()
         if not self.started.is_set():
             logger.info("Starting camera stream")
             self.started.set()
 
-    def disconnect(self, camera_type: CameraType, websocket: WebSocket) -> None:
+    async def disconnect(self, camera_type: CameraType, websocket: WebSocket) -> None:
+        await self.lock.acquire()
         self.web_sockets[camera_type].remove(websocket)
         if not self.web_sockets[camera_type]:
             self.web_sockets.pop(camera_type)
+        self.lock.release()
         if not self.web_sockets:
             logger.info("Stopping camera stream")
             self.started.clear()
 
+    @classmethod
+    @functools.lru_cache
+    def get(cls) -> "ConnectionManager":
+        return cls()  # Returns a singleton to use for all connections.
+
+
+async def safe_send(ws: WebSocket, img_bytes: bytes) -> None:
+    try:
+        await asyncio.wait_for(ws.send_bytes(img_bytes), 1.0)
+    except Exception:
+        logger.exception("Exception while sending websocket bytes")
+
+
+async def safe_send_many(wss: Iterable[WebSocket], img_bytes: bytes) -> None:
+    await asyncio.gather(*(safe_send(ws, img_bytes) for ws in wss), return_exceptions=True)
+
 
 async def run_connection_manager(manager: ConnectionManager) -> None:
-    buffer = io.BytesIO()
+    rgb_buffer = io.BytesIO()
+    depth_buffer = io.BytesIO()
 
     while True:
         await manager.started.wait()
 
-        async for frame in iter_realsense_frames("rgb"):
-            img = frame.to_image()
-            img.save(buffer, format="JPEG")
-            img_bytes = buffer.getvalue()
-            buffer.seek(0)
+        async for frame in iter_frames():
+            rgb_img = frame.rgb.to_image()
+            depth_img = frame.depth.to_image()
+
+            rgb_img.save(rgb_buffer, format="JPEG")
+            rgb_img_bytes = rgb_buffer.getvalue()
+            rgb_buffer.seek(0)
+
+            depth_img.save(depth_buffer, format="JPEG")
+            depth_img_bytes = depth_buffer.getvalue()
+            depth_buffer.seek(0)
+
             if not manager.started.is_set():
                 break
+
+            await manager.lock.acquire()
             await asyncio.gather(
-                *(
-                    ws.send_bytes(img_bytes)
-                    for wss in manager.web_sockets.values()
-                    for ws in wss
-                )
+                safe_send_many(manager.web_sockets.get("rgb", {}), rgb_img_bytes),
+                safe_send_many(manager.web_sockets.get("depth", {}), depth_img_bytes),
+                return_exceptions=True,
             )
+            manager.lock.release()
             await asyncio.sleep(0.01)
-
-
-connections = ConnectionManager()
 
 
 @r.websocket("/{camera}/ws")
@@ -180,7 +225,7 @@ async def get_camera_video_feed(ws: WebSocket, camera: str) -> None:
     camera_type = cast_camera_type(camera)
 
     try:
-        await connections.connect(camera_type, ws)
+        await ConnectionManager.get().connect(camera_type, ws)
 
         # Receive until the websocket is closed by the client.
         while True:
@@ -192,7 +237,7 @@ async def get_camera_video_feed(ws: WebSocket, camera: str) -> None:
         logger.exception("Web socket is disconnected")
 
     finally:
-        connections.disconnect(camera_type, ws)
+        await ConnectionManager.get().disconnect(camera_type, ws)
 
 
 async def test_iter_frames(total_seconds: float = 3.0) -> None:
@@ -218,12 +263,13 @@ async def test_iter_frames(total_seconds: float = 3.0) -> None:
 
     i = 0
 
-    async for frame in iter_realsense_frames("depth"):
+    async for frame in iter_frames():
+        depth = frame.depth
         i += 1
         if i > total_frames:
             break
-        logger.info("Captured frame %d / %d with shape (%d, %d)", i, total_frames, frame.height, frame.width)
-        for p in ovstream.encode(frame):
+        logger.info("Captured frame %d / %d with shape (%d, %d)", i, total_frames, depth.height, depth.width)
+        for p in ovstream.encode(depth):
             output.mux(p)
 
     for p in ovstream.encode():
--- stretch/cpp/realsense/lib.cpp
+++ stretch/cpp/realsense/lib.cpp
@@ -16,159 +16,75 @@ void check_error(rs2_error* e) {
     }
 }
 
-enum CameraType { rgb = 0, depth };
-
-class Frame {
+template <class frame_t, typename data_t>
+class BaseFrame {
    protected:
-    CameraType camera_type;
-    rs2_frame* frame;
+    frame_t frame;
 
    public:
-    Frame(rs2_frame* frames, int frame_id, CameraType camera_type) : camera_type(camera_type) {
-        rs2_error* e = 0;
-        frame = rs2_extract_frame(frames, frame_id, &e);
-        check_error(e);
-        rs2_keep_frame(frame);
-    }
+    BaseFrame(frame_t& frame) : frame(std::forward<frame_t>(frame)) {}
 
-    ~Frame() { rs2_release_frame(frame); }
+    const data_t* data() const { return (const data_t*)frame.get_data(); }
+    const int width() const { return frame.get_width(); }
+    const int height() const { return frame.get_height(); }
+    const int bytes_per_pixel() const { return frame.get_bytes_per_pixel() / sizeof(data_t); }
+    const unsigned long long frame_number() const { return frame.get_frame_number(); }
+    const rs2_time_t frame_timestamp() const { return frame.get_timestamp(); }
+};
 
-    const uint8_t* data() const {
-        rs2_error* e = 0;
-        auto data = (const uint8_t*)(rs2_get_frame_data(frame, &e));
-        check_error(e);
-        return data;
-    }
+class ColorFrame : public BaseFrame<rs2::video_frame, uint8_t> {
+   public:
+    ColorFrame(rs2::video_frame& frame) : BaseFrame(frame) {}
+};
 
-    const int width() const {
-        rs2_error* e = 0;
-        auto frame_width = rs2_get_frame_width(frame, &e);
-        check_error(e);
-        return frame_width;
-    }
+class DepthFrame : public BaseFrame<rs2::depth_frame, uint8_t> {
+   public:
+    DepthFrame(rs2::depth_frame& frame) : BaseFrame(frame) {}
 
-    const int height() const {
-        rs2_error* e = 0;
-        auto frame_height = rs2_get_frame_height(frame, &e);
-        check_error(e);
-        return frame_height;
-    }
+    const float units() const { return frame.get_units(); }
+};
 
-    const int bytes_per_pixel() const {
-        switch (camera_type) {
-            case rgb:
-                return 2;
-            case depth:
-                return 2;  // Z16 has two bytes for Z data.
-            default:
-                throw std::runtime_error("Unsupported camera type for frame");
-        }
-    }
+class FrameSet {
+   public:
+    ColorFrame rgb;
+    DepthFrame depth;
 
-    const unsigned long long frame_number() const {
-        rs2_error* e = 0;
-        auto frame_number = rs2_get_frame_number(frame, &e);
-        check_error(e);
-        return frame_number;
-    }
-
-    const rs2_time_t frame_timestamp() const {
-        rs2_error* e = 0;
-        auto frame_timestamp = rs2_get_frame_timestamp(frame, &e);
-        check_error(e);
-        return frame_timestamp;
-    }
+    FrameSet(rs2::video_frame& rgb, rs2::depth_frame& depth) : rgb(rgb), depth(depth) {}
 };
 
 class FrameGenerator {
    private:
-    CameraType camera_type;
-    size_t device_id;
-    rs2_context* ctx;
-    rs2_device_list* device_list;
-    rs2_device* dev;
-    rs2_pipeline* pipeline;
-    rs2_config* config;
-    rs2_pipeline_profile* pipeline_profile;
-    rs2_frame* frames;
-    rs2_frame* frame;
-    int num_frames;
-    int frame_id;
+    rs2::pipeline pipe;
 
    public:
-    FrameGenerator(CameraType camera_type, size_t device_id = 0)
-        : camera_type(camera_type), device_id(device_id), frame_id(0) {
-        rs2_error* e = 0;
-        ctx = rs2_create_context(RS2_API_VERSION, &e);
-        check_error(e);
-        device_list = rs2_query_devices(ctx, &e);
-        check_error(e);
-        int dev_count = rs2_get_device_count(device_list, &e);
-        check_error(e);
-        if (static_cast<int>(device_id) >= dev_count) {
+    FrameGenerator(size_t device_id = 0) {
+        // Gets the correct device.
+        rs2::context ctx;
+        auto device_list = ctx.query_devices();
+        auto device_count = device_list.size();
+        if (device_id >= device_count) {
             std::ostringstream ss;
-            ss << "Device ID " << device_id << " is out-of-bounds since only " << dev_count
+            ss << "Device ID " << device_id << " is out-of-bounds since only " << device_count
                << "device(s) are connected";
             throw std::runtime_error(ss.str());
         }
-        dev = rs2_create_device(device_list, device_id, &e);
-        check_error(e);
-        pipeline = rs2_create_pipeline(ctx, &e);
-        check_error(e);
-        config = rs2_create_config(&e);
-        check_error(e);
-        switch (camera_type) {
-            case rgb:
-                rs2_config_enable_stream(config, RS2_STREAM_COLOR, 0, /* width */ 640, /* height */ 480,
-                                         RS2_FORMAT_YUYV,
-                                         /* fps */ 30, &e);
-                break;
-            case depth:
-                rs2_config_enable_stream(config, RS2_STREAM_DEPTH, 0, /* width */ 640, /* height */ 480, RS2_FORMAT_Z16,
-                                         /* fps */ 30, &e);
-                break;
-            default:
-                throw std::runtime_error("Unsupported camera type");
-        }
-        check_error(e);
-        pipeline_profile = rs2_pipeline_start_with_config(pipeline, config, &e);
-        check_error(e);
-        frames = rs2_pipeline_wait_for_frames(pipeline, 1000 /* RS2_DEFAULT_TIMEOUT */, &e);
-        check_error(e);
-        num_frames = rs2_embedded_frames_count(frames, &e);
-        check_error(e);
-        if (num_frames <= 0) throw std::runtime_error("Didn't get any frames");
-        frame = rs2_extract_frame(frames, frame_id++, &e);
-        check_error(e);
-    }
+        auto device = device_list[device_id];
 
-    ~FrameGenerator() {
-        rs2_error* e = 0;
-        rs2_release_frame(frames);
-        rs2_pipeline_stop(pipeline, &e);
-        check_error(e);
-        rs2_delete_pipeline_profile(pipeline_profile);
-        rs2_delete_config(config);
-        rs2_delete_pipeline(pipeline);
-        rs2_delete_device(dev);
-        rs2_delete_device_list(device_list);
-        rs2_delete_context(ctx);
+        // Creates a new pipeline.
+        pipe = rs2::pipeline(ctx);
+        rs2::config cfg;
+        cfg.enable_stream(RS2_STREAM_COLOR, 0, /* width */ 640, /* height */ 480, RS2_FORMAT_YUYV, /* fps */ 30);
+        cfg.enable_stream(RS2_STREAM_DEPTH, 0, /* width */ 640, /* height */ 480, RS2_FORMAT_Z16, /* fps */ 30);
+        pipe.start(cfg);
     }
 
     const FrameGenerator* iter() { return this; }
 
-    const Frame* next() {
-        rs2_error* e = 0;
-        if (frame_id >= num_frames) {
-            rs2_release_frame(frames);
-            frames = rs2_pipeline_wait_for_frames(pipeline, 1000 /* RS2_DEFAULT_TIMEOUT */, &e);
-            check_error(e);
-            num_frames = rs2_embedded_frames_count(frames, &e);
-            check_error(e);
-            frame_id = 0;
-        }
-        if (frame_id >= num_frames) throw std::runtime_error("This should never happen");
-        return new Frame(frames, frame_id++, camera_type);
+    const FrameSet* next() {
+        auto frames = pipe.wait_for_frames();
+        auto color_frame = frames.get_color_frame();
+        auto depth_frame = frames.get_depth_frame();
+        return new FrameSet(color_frame, depth_frame);
     }
 };
 
@@ -176,8 +92,6 @@ using namespace pybind11::literals;
 
 PYBIND11_MODULE(lib, m) {
     m.def("device_count", &device_count);
-
-    pybind11::enum_<CameraType>(m, "CameraType").value("rgb", rgb).value("depth", depth).export_values();
 
     pybind11::class_<rs2_quaternion>(m, "Quaternion")
         .def_readonly("x", &rs2_quaternion::x)
@@ -190,8 +104,8 @@ PYBIND11_MODULE(lib, m) {
         .def_readonly("y", &rs2_vector::y)
         .def_readonly("z", &rs2_vector::z);
 
-    pybind11::class_<Frame>(m, "Frame", pybind11::buffer_protocol())
-        .def_buffer([](Frame& m) -> pybind11::buffer_info {
+    pybind11::class_<ColorFrame>(m, "ColorFrame", pybind11::buffer_protocol())
+        .def_buffer([](ColorFrame& m) -> pybind11::buffer_info {
             auto buf = pybind11::buffer_info(m.data(), sizeof(uint8_t), true);
             buf.format = pybind11::format_descriptor<uint8_t>::format();
             buf.ndim = 3;
@@ -199,14 +113,32 @@ PYBIND11_MODULE(lib, m) {
             buf.strides = {m.width() * m.bytes_per_pixel(), m.bytes_per_pixel(), 1};
             return buf;
         })
-        .def_property_readonly("width", &Frame::width)
-        .def_property_readonly("height", &Frame::height)
-        .def_property_readonly("bytes_per_pixel", &Frame::bytes_per_pixel)
-        .def_property_readonly("frame_number", &Frame::frame_number)
-        .def_property_readonly("frame_timestamp", &Frame::frame_timestamp);
+        .def_property_readonly("width", &ColorFrame::width)
+        .def_property_readonly("height", &ColorFrame::height)
+        .def_property_readonly("bytes_per_pixel", &ColorFrame::bytes_per_pixel)
+        .def_property_readonly("frame_number", &ColorFrame::frame_number)
+        .def_property_readonly("frame_timestamp", &ColorFrame::frame_timestamp);
+
+    pybind11::class_<DepthFrame>(m, "DepthFrame", pybind11::buffer_protocol())
+        .def_buffer([](DepthFrame& m) -> pybind11::buffer_info {
+            auto buf = pybind11::buffer_info(m.data(), sizeof(uint8_t), true);
+            buf.format = pybind11::format_descriptor<uint8_t>::format();
+            buf.ndim = 3;
+            buf.shape = {m.height(), m.width(), m.bytes_per_pixel()};
+            buf.strides = {m.width() * m.bytes_per_pixel(), m.bytes_per_pixel(), 1};
+            return buf;
+        })
+        .def_property_readonly("width", &DepthFrame::width)
+        .def_property_readonly("height", &DepthFrame::height)
+        .def_property_readonly("bytes_per_pixel", &DepthFrame::bytes_per_pixel)
+        .def_property_readonly("frame_number", &DepthFrame::frame_number)
+        .def_property_readonly("frame_timestamp", &DepthFrame::frame_timestamp)
+        .def_property_readonly("units", &DepthFrame::units);
+
+    pybind11::class_<FrameSet>(m, "Frame").def_readonly("rgb", &FrameSet::rgb).def_readonly("depth", &FrameSet::depth);
 
     pybind11::class_<FrameGenerator>(m, "FrameGenerator")
-        .def(pybind11::init<CameraType>(), "camera_type"_a)
+        .def(pybind11::init<>())
         .def("__iter__", &FrameGenerator::iter)
         .def("__next__", &FrameGenerator::next);
 }
